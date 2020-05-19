8a94fc3ad83803cb46d383ff4eddbdbe27baa130
Balasubramanian <bala_465@rediffmail.com>
2020-05-19 14:50:40 +0530

    Refactored : Extracted tennis game instance in test class for reusability

--- src/test/java/com/tdd/tennis/TennisGameTest.java
+++ src/test/java/com/tdd/tennis/TennisGameTest.java
@@ -3,21 +3,26 @@ package com.tdd.tennis;
 import static org.junit.Assert.assertThat;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest {
 
+	TennisGame tennisGame;
+
+	@Before
+	public void initialize() {
+		tennisGame = new TennisGame();
+	}
+
 	@Test
 	public void playerScoreShouldBeZeroBeforeGameStarts() {
-
-		TennisGame tennisGame = new TennisGame();
 
 		assertThat(tennisGame.getPlayerOneScoredPoint(), CoreMatchers.is(0));
 	}
 
 	@Test
 	public void firstPlayerNameShouldBeInitializedAsPlayerOneWhenGameStarts() {
-		TennisGame tennisGame = new TennisGame();
 
 		assertThat(tennisGame.getPlayerOneName(), CoreMatchers.is("Player One"));
 
